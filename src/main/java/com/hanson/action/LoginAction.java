50c35aa57a4ac3c30ca3c738f8435ca151a7fc47
Hanson96 <17764590109@163.com>
2017-03-26 19:29:30 +0800

    .

--- src/main/java/com/hanson/action/LoginAction.java
+++ src/main/java/com/hanson/action/LoginAction.java
@@ -4,19 +4,22 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.struts2.interceptor.ServletRequestAware;
+
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class LoginAction extends ActionSupport{
+public class LoginAction extends ActionSupport implements ServletRequestAware{
 	
+	private HttpServletRequest request;
 	private String userName;
-	
 	private String password;
 	
 	@Override
 	public String execute() throws Exception {
 		// TODO Auto-generated method stub
-		ActionContext ctx = ActionContext.getContext();
+		ActionContext ctx = ActionContext.getContext();System.out.println("username:"+userName);
+		System.out.println("password:"+request.getParameter("password"));
 		Map request_a = (Map)ctx.get("request");
 		request_a.put("name", "hhh");
 		if("hanson".equals(userName)){
@@ -25,4 +28,19 @@ public class LoginAction extends ActionSupport{
 			return this.ERROR;
 		}
 	}
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+	
+	
 }
